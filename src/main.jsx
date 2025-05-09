110beb4d9c35c0d90f24ed7adb1fb75eec44ea65
Tim Combs <153787426+tlcombs6@users.noreply.github.com>
2025-05-08 23:09:30 -0500

    login

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style.css';
 
+const PUBLISHABLE_KEY = "your-clerk-publishable-key"; // replace this
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 );
