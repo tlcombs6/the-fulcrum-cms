7483a3c5a1b0309c7e52637b169c46de04506a19
Tim Combs <153787426+tlcombs6@users.noreply.github.com>
2025-05-13 23:23:41 -0500

    supabase env and js changes

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://goqfxkfdkiefwowcpcwu.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
